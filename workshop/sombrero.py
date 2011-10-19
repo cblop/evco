05cce92be8ba5fc6e80105ee5c666158a6a45561
Commander Blop <commanderblop@gmail.com>
2011-10-19 21:34:48 +0100

    made the sombrero more ripply

--- workshop/sombrero.py
+++ workshop/sombrero.py
@@ -1,19 +1,17 @@
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
-from matplotlib.colors import LogNorm
 from matplotlib.ticker import LinearLocator, FormatStrFormatter
 import matplotlib.pyplot as plt
 import numpy as np
 
 def sombrero(c=None):
     if c:
-        R = np.sqrt((c[0]-2*np.exp(1))**2 + (c[1]-np.pi)**2)
+        R = (c[0]-2*np.exp(1))**2 + (c[1]-np.pi)**2 + 1
         Z = np.sin(1) - (np.sin(R)/R)
         return Z
     else:
         fig = plt.figure()
         ax = fig.gca(projection='3d')
-        #ax = Axes3D(fig, azim = -128, elev = 43)
         X = np.arange(0, 10, 0.2)
         Y = np.arange(0, 10, 0.2)
         X, Y = np.meshgrid(X, Y)
@@ -21,10 +19,6 @@ def sombrero(c=None):
 
         #plot_surface(linewidth=0) or plot_wireframe
         surf = ax.plot_surface(X, Y, Z, rstride=1, cstride=1, linewidth=0, cmap=cm.jet)
-        #ax.set_zlim(-1.01, 1.01)
-
-        #ax.zaxis.set_major_locator(LinearLocator(10))
-        #ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
 
         #fig.colorbar(surf, shrink=0.5, aspect=5)
 
